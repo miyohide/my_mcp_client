13288b93fea6e13630fb3439a618856e39fefcb4
Hidenori Miyoshi <miyohide@gmail.com>
2026-03-19 21:42:17 +0900

    Spring Initializrを使って初期アプリを作成

---
+++ src/main/java/com/github/miyohide/mymcp/MymcpApplication.java
@@ -0,0 +1,13 @@
+package com.github.miyohide.mymcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MymcpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MymcpApplication.class, args);
+	}
+
+}
